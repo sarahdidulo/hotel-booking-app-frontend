19f22668e9cbe826ba8ec764e189eeb98b05804d
sarahdidulo <didulo.sarah@gmail.com>
2026-04-01 13:39:24 +0800

    new app

---
+++ trivia-game-maker/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className="landing-page">
+          <h1>Trivia Game Maker</h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
